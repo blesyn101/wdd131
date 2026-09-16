ecdad5f7a6251077676cd446780c98a054ec4999
Agharese Blessing <blessing4coding@gmail.com>
2026-09-16 03:35:05 +0100

    fix responsive menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,12 @@ const today = new Date();
 
 currentyear.innerHTML = today.getFullYear();
 lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
+
+const menubutton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menubutton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    menubutton.classList.toggle("open");
+});
